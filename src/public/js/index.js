08bd4035efc572709482644a21f88c177c8df544
Marttheus <marttheusf@gmail.com>
2020-03-10 00:02:40 -0300

    add players

---
+++ src/public/js/index.js
@@ -0,0 +1,12 @@
+$(function () {
+    $("#add").click(function () {
+        var names = $("#player").val();
+        if(names !== null && names !== undefined && names !== ""){
+            names.split(',').forEach((player, index) => {
+                var content = $(".content");
+                content.append("<div class='item'>" + player.trim() + "</div>");
+            });
+            $("#player").val("");;
+        }
+    });
+ });
